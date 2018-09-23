1881ecf34ed4a423ed12f33d2db4940a96aa4ba6
Hudson Schumaker <hudson@schumakerteam.com>
2018-09-23 01:45:35 -0300

    Classe Aplicacao, usa a anotação @SpringBootApplication.

---
+++ src/main/java/br/com/schumaker/spring/Aplicacao.java
@@ -0,0 +1,15 @@
+package br.com.schumaker.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author hudsonschumaker
+ */
+@SpringBootApplication
+public class Aplicacao {
+    public static void main(String... args) {
+        SpringApplication.run(Aplicacao.class, args);
+    }
+}
