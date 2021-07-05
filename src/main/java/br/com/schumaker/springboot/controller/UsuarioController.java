b60204364e52ba2282ef3fd611b21a383b2ca9ac
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-07-05 20:37:49 +0200

    Update UsuarioController.java

--- src/main/java/br/com/schumaker/springboot/controller/UsuarioController.java
+++ src/main/java/br/com/schumaker/springboot/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
- * @author hudsonschumaker
+ * @author hudson schumaker
  */
 @RestController
 public class UsuarioController {
